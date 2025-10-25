dbb19cc5682d1fec4d689bd218fd9cffe5296693
farnood <farnoodMIT90@gmail.com>
2025-10-25 09:05:03 +0330

    nutrition

---
+++ week2/nutrition.py
@@ -0,0 +1,22 @@
+Fruits = {'Appele':130,
+          'Avocado':50,
+          'banana':110,
+          'Cantaloupe':50,
+          'Greapfruit':60,
+          'Greaps':90,
+          'Honeydewmelon':50,
+          'Kiwifruit':90,
+          'Lemon':15,
+          'Lime':20,
+          'Nectarine':60,
+          'Orange':80,
+          'Peach':60,
+          'Pear':100,
+          'Pineappel':50,
+          'Plums':70,
+          'Strawberries':100,
+          'Tangerine':50,
+          'Watermelon':80
+          }
+user_input = input('item: ').lower()
+print(Fruits[user_input])
